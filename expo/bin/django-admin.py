7307a23888bd9deb00ba8f2d7b7d8fff9c73f7fa
peppapi21 <fias2001@hotmail.com>
2019-09-23 21:09:22 -0300

    Primer act del proyecto para la expo

---
+++ expo/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/programacion/proyecto/expo/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
---
+++ preguntadas/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class PreguntadasConfig(AppConfig):
+    name = 'preguntadas'
